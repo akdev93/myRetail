f53670ef6b7fd16e1ebec1edc51a79dc9b767f11
koneria <koneria@amazon.com>
2016-04-23 15:37:46 -0700

    added customer notfoundexception and removed its mapper

---
+++ myRetailApi/src/main/java/com/myRetail/product/resources/NotFoundException.java
@@ -0,0 +1,27 @@
+package com.myRetail.product.resources;
+
+import com.myRetail.product.model.Error;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by koneria on 4/23/16.
+ */
+public class NotFoundException extends javax.ws.rs.NotFoundException {
+
+    public NotFoundException(String message) {
+        super(Response.status(Response.Status.NOT_FOUND)
+                .entity(new Error(new java.util.Date(), message))
+                .type(MediaType.APPLICATION_JSON).build());
+    }
+    public NotFoundException(Response r) {
+        super(r);
+    }
+
+    public NotFoundException(String message, Throwable th) {
+         super(Response.status(Response.Status.NOT_FOUND)
+                .entity(new Error(new java.util.Date(), message))
+                .type(MediaType.APPLICATION_JSON).build(),th);
+    }
+}
